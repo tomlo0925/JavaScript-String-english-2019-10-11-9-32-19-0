1d507784395320ef64663b620ba79c3cc52803f7
tomlo0925 <tomlo0925@gmail.com>
2020-11-19 17:53:58 +0800

    feat: complete assignment

--- index.js
+++ index.js
@@ -2,12 +2,24 @@
 var name = 'hello';
 // TODO write your code here...
 
+console.log(name.toUpperCase());
 
 // Complete the following procedure, capitalizing all the words in the variable sentence and printing: 'Good Afternoon, Mr Mike.'
 var sentence = 'good afternoon, mr mike.';
 //TODO write your code here...
+const capitalizeFirstLetter = (str) => {
+  const word_arr = str.split(' ');
+  const result_arr = [];
+  word_arr.forEach(([firstLetter, ...remaining]) => {
+    let capitalized = firstLetter.toUpperCase() + remaining.join('');
+    result_arr.push(capitalized);
+  })
 
+  return result_arr.join(' ');
+};
+console.log(capitalizeFirstLetter(sentence));
 
 // Complete the following procedure to extract the amount of money variable, output: 20.
 var money = '￥20';
 //TODO write your code here...
+console.log(money.substr(1,2));
